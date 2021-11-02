ecb47832999c4f8b1977a989386bc987436c14b4
Seth Rylan Gainey <seth@apothesource.com>
2021-11-01 21:28:17 -0400

    sscmt updates.

--- sscmt/bootstrap.sh
+++ sscmt/bootstrap.sh
@@ -3,7 +3,6 @@ set -e
 
 apt-get update
 apt-get install -y ruby-full curl
-gem update
 mkdir -p /opt/sscmt/
 curl -L https://sethrylan.org/sscmt/sscmt.rb --output /opt/sscmt/sscmt.rb
 chmod +x /opt/sscmt/sscmt.rb
