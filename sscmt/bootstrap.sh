c290460a5f79eb52e8bd762dec1275b24d54fcd8
Seth Rylan Gainey <seth@apothesource.com>
2021-11-01 20:49:26 -0400

    Add sscmt files for hosting.

---
+++ sscmt/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+apt-get update
+apt-get install -y ruby-full curl
+gem update
+mkdir -p /opt/sscmt/
+curl https://sethrylan.org/files/sscmt.rb --output /opt/sscmt/sscmt.rb
+chmod +x /opt/sscmt/sscmt.rb
