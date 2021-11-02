c0b506158bac42d956767fa8db7855edc99d0b60
Seth Rylan Gainey <seth@apothesource.com>
2021-11-01 21:13:48 -0400

    Fix path.

--- sscmt/bootstrap.sh
+++ sscmt/bootstrap.sh
@@ -5,5 +5,5 @@ apt-get update
 apt-get install -y ruby-full curl
 gem update
 mkdir -p /opt/sscmt/
-curl -L https://sethrylan.org/files/sscmt.rb --output /opt/sscmt/sscmt.rb
+curl -L https://sethrylan.org/sscmt/sscmt.rb --output /opt/sscmt/sscmt.rb
 chmod +x /opt/sscmt/sscmt.rb
