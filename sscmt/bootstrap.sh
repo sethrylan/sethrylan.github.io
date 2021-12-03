82d00c136366de2a595d0d6db8f7e571f51a52ec
Seth Gainey <seth.rylan@gmail.com>
2021-12-03 15:32:51 -0500

    Cleanup.

--- sscmt/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-apt-get update
-apt-get install -y ruby-full curl
-mkdir -p /opt/sscmt/
-curl -L https://sethrylan.org/sscmt/sscmt.rb --output /opt/sscmt/sscmt.rb
-chmod +x /opt/sscmt/sscmt.rb
